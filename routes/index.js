23638e671340182b199b57cb1d00dc4444baed9e
kievo23 <kelvinchege@gmail.com>
2017-04-26 11:58:18 +0300

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var connection = require(__dirname + '/../config/db'); 
 var moment = require('moment');
+var Candidate = require(__dirname + '/../models/Candidate');
+var Region = require(__dirname + '/../models/Region');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +12,26 @@ router.get('/', function(req, res, next) {
   var endOfLastSevenDays = moment().subtract(7,'days');
   var startOfLastWeek = moment().subtract(1,'weeks').startOf('isoweek');
   var endOfLastWeek = moment().subtract(1,'weeks').endOf('isoweek');
+
+  var candidates = Candidate.findAll({ 
+	    where: {
+			position: {
+				$notLike: 'president'
+			}
+		},
+		include: [
+	        { model: Region, as: 'region'}
+	    ]
+	});
+
+	var presidents = Candidate.findAll({ 
+		where: {
+			position: 'president'
+		},
+	    include: [
+	        { model: Region, as: 'region'}
+	    ]
+	});
 
   var yesterday = connection.query('select count(distinct "userId"),candidateid,c.name,c.odd,c.position from betdetails as d \
 	left join bets as b on b.id = d.betid \
@@ -38,7 +60,7 @@ router.get('/', function(req, res, next) {
 	group by candidateid, c.name, c.odd,c.position',
 	      { type: connection.QueryTypes.SELECT }
     );
-  Promise.all([yesterday,lastSevenDays, lastweek, overall]).then(values => {
+  Promise.all([yesterday,lastSevenDays, lastweek, overall, candidates, presidents]).then(values => {
   		console.log(values[0]);
 	  res.render('index', { 
 	  	title: 'Kenyan Elections Virtual Bets',
@@ -46,7 +68,9 @@ router.get('/', function(req, res, next) {
 	  	yesterdays: values[0],
 	  	lastSevenDays: values[1],
 	  	lastweek: values[2],
-	  	overall: values[3]
+	  	overall: values[3],
+	  	candidates: values[4],
+	  	presidents: values[5]
 	  });
   });
 });
